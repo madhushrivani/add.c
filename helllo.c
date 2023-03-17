d3e0c3a1fc3b2dd0555d9cd24b68aad959b9fcb0
madhushrivani <madhushrivani@gmail.com>
2023-03-17 21:53:13 +0530

    a new file

---
+++ helllo.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int i;
+    printf("enter a number %d", i);
+    scanf("%d", &i);
+        for (int n=1; n<11; n++){
+
+        printf("%d*%d=%d\n", i*n );
+    }
+    printf("hello world");
+    return 0;
+}
