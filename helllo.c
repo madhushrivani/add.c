f737bf2469d4c64aa1ed6dc50ee317a1ddee845f
madhushrivani <madhushrivani@gmail.com>
2023-03-18 23:29:38 +0530

    universe

--- helllo.c
+++ helllo.c
@@ -7,7 +7,7 @@ int main()
     scanf("%d", &i);
         for (int n=1; n<11; n++){
 
-        printf("%d*%d=%d\n", i*n );
+        printf("%d*%d=%d\n3", i*n );
     }
     printf("hello world");
     return 0;
